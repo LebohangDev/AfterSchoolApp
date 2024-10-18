c50c282293d83dea3f0e8c2dc4a9b473b915f3e1
Lebohang Khasipe <lkhasipe39@gmail.com>
2024-10-18 16:27:08 +0400

    added cart functionality and checkout section (second commit)

---
+++ JS/Activities.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+let Activities =[
+    {
+        name: 'Football',
+        id: 1,
+        img: '/Images/Activities-IMG/football.jpg',
+        location: 'TOKYO',
+        space: '6',
+        price: '23.30',
+        isPopular: true
+    },
+    {
+        name: 'Basketball',
+        id: 2,
+        img: '/Images/Activities-IMG/basketball.jpg',
+        location: 'OSAKA',
+        space: '9',
+        price: '20.30',
+        isPopular: true
+    },
+    {
+        name: 'Archery',
+        id: 3,
+        img: '/Images/Activities-IMG/—Pngtree—basic basketball drawing_14543340.png',
+        location: 'TOKYO',
+        space: '14',
+        price: '16.50',
+        isPopular: false
+    },
+    {
+        name: 'Volleyball',
+        id: 4,
+        img: '/Images/Activities-IMG/Volleyball.jpg',
+        location: 'SHIBUYA',
+        space: '7',
+        price: '19.30',
+        isPopular: true
+    },
+    {
+        name: 'Swimming',
+        id: 5,
+        img: '/Images/Activities-IMG/swimming.jpg',
+        location: 'TOKYO',
+        space: '3',
+        price: '19.30',
+        isPopular: true
+    },
+    {
+        name: 'Baseball',
+        id: 6,
+        img: '/Images/Activities-IMG/baseball.jpg',
+        location: 'OSAKA',
+        space: '6',
+        price: '25.30',
+        isPopular: true
+    }
+    
+
+]
